7e1f6347b088f6bafff02fd5481b6901bfbe8c9a
vanshjain2060 <vanshjain0311@gmail.com>
2025-06-29 20:54:39 +0530

    Testing  Mockito

---
+++ Week2/Testing/Mockito/Mockito/src/test/java/org/example/MyServiceTest.java
@@ -0,0 +1,29 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.mockito.Mockito.*;
+import org.mockito.Mockito;
+
+public class MyServiceTest {
+
+    @Test
+    public void testExternalApi() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        when(mockApi.getData()).thenReturn("Mock Data");
+
+        MyService service = new MyService(mockApi);
+        String result = service.fetchData();
+
+        assertEquals("Mock Data", result);
+    }
+
+    @Test
+    public void testVerifyInteraction() {
+        ExternalApi mockApi = Mockito.mock(ExternalApi.class);
+        MyService service = new MyService(mockApi);
+        service.fetchData();
+        verify(mockApi).getData();
+    }
+}
