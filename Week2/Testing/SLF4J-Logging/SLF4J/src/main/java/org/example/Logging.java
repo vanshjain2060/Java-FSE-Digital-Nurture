4f0bb1463a0be7f969b449d7140fe99ea75dca1d
vanshjain2060 <vanshjain0311@gmail.com>
2025-06-29 21:00:02 +0530

    Testing SL4J Logging

--- Week2/Testing/SLF4J-Logging/SLF4J/src/main/java/org/example/Logging.java
+++ Week2/Testing/SLF4J-Logging/SLF4J/src/main/java/org/example/Logging.java
@@ -7,7 +7,7 @@ public class Logging {
     private static final Logger logger = LoggerFactory.getLogger(Logging.class);
 
     public static void main(String[] args) {
-        logger.error("This is an error message");
-        logger.warn("This is a warning message");
+        logger.error("This is an error message.");
+        logger.warn("This is a warning message.");
     }
 }
