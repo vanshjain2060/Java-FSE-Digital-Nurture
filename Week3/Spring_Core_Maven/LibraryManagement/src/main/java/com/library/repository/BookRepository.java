43db1abce30a6ccef5b5e209822fa9578b50c4e1
vanshjain2060 <vanshjain0311@gmail.com>
2025-07-03 23:11:24 +0530

    Week3 Implementing Dependency Injection

--- Week3/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
+++ Week3/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/repository/BookRepository.java
@@ -1,5 +1,8 @@
 package com.library.repository;
 
 public class BookRepository {
+    public void saveBook(String bookName) {
+        System.out.println("Book saved: " + bookName);
+    }
 }
 
--- Week3/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
+++ Week3/Spring_Core_Maven/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -1,4 +1,18 @@
 package com.library.service;
 
+import com.library.repository.BookRepository;
+
 public class BookService {
+
+    private BookRepository bookRepository;
+
+    // Setter for Dependency Injection
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    public void addBook(String name) {
+        System.out.println("Adding book...");
+        bookRepository.saveBook(name);
+    }
 }
